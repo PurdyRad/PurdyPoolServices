8e7e010afc445a23d8cd981f3ac4e9e0e81d2a37
PurdyRad <adampurdy407@gmail.com>
2021-12-14 21:29:27 -0500

    finished form and schema

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,31 +1,82 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import * as yup from 'yup';
+import inquirySchema from '../inquirySchema';
 
 const initialFormValues = {
     name: '',
     phoneNumber: '',
     email: '',
     inquiry: ''
-}
+};
 
 const Contact = () => {
     const [formValues, setFormValues] = useState(initialFormValues);
+    const [formErrors, setFormErrors] = useState('');
+    const [disablo, setDisablo] = useState(true);
+
+    useEffect(() => {
+        if (formValues.name
+             && formValues.phoneNumber
+              && formValues.email
+               && formValues.inquiry) {
+            inquirySchema
+            .isValid(formValues)
+            .then((valid) => setDisablo(!valid));
+        } else {
+            setDisablo(true)
+        }
+    }, [formValues]);
 
     const changer = (e) => {
-        setFormValues({formValues: e.target})
-    }
+        yup.reach(inquirySchema, e.target.name)
+            .validate(e.target.value)
+            .then(() => {
+                setFormErrors({...formErrors, [e.target.name]: ''})
+            })
+            .catch(err => {
+                setFormErrors({...formErrors,
+                     [e.target.name]: err.errors[0]})
+            });
+        setFormValues({...formValues, [e.target.name]:e.target.value});
+    };
 
     return (
         <div>
             <h4>If you have any customer service related inquires please feel free to reach out to us. We will get back to you as soon as possible, thank you!</h4>
             <form>
-                <label>Your Name *</label>
-                <input value={formValues.name} onChange={changer}></input>
-                <label>Your Phone Number *</label>
-                <input value={formValues.phoneNumber} onChange={changer}></input>
-                <label> Your Email *</label>
-                <input value={formValues.email} onChange={changer}></input>
-                <label>How can we assist you *</label>
-                <input value ={formValues.inquiry} onChange={changer}></input>
+                <label>Your Name *
+                <input
+                 type='text'
+                  name='name'
+                   value={formValues.name}
+                    onChange={changer}/>
+                    <div>{formErrors.name}</div>
+                </label>
+                <label>Your Phone Number *
+                <input
+                 type='text'
+                  name='phoneNumber'
+                   value={formValues.phoneNumber}
+                    onChange={changer}/>
+                    <div>{formErrors.phoneNumber}</div>
+                </label>
+                <label> Your Email *
+                <input
+                 type='email'
+                  name='email'
+                   value={formValues.email}
+                    onChange={changer}/>
+                    <div>{formErrors.email}</div>
+                </label>
+                <label>How can we assist you *
+                <input
+                 type='text'
+                  name='inquiry'
+                   value={formValues.inquiry}
+                    onChange={changer}/>
+                    <div>{formErrors.inquiry}</div>
+                </label>
+                <button disabled={disablo}>Submit</button>
             </form>
         </div>
     )
